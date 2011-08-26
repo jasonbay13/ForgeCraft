ea94c1eb936a1da30ed46adaac42c15db14b82c1
Brayden Lopez <headdetective2@gmail.com>
2011-08-25 20:33:27 -0700

    Added port checker and weather shiz

--- SMP/Command/Commands/CmdRain.cs
+++ SMP/Command/Commands/CmdRain.cs
@@ -6,7 +6,7 @@ namespace SMP
     public class CmdRain : Command
     {
         public override string Name { get { return "rain"; } }
-        public override List<string> Shortcuts { get { return new List<string> {  }; } }
+        public override List<string> Shortcuts { get { return new List<string> { }; } }
         public override string Category { get { return "other"; } }
         public override bool ConsoleUseable { get { return false; } }
         public override string Description { get { return "Makes it rain?"; } }
@@ -15,50 +15,65 @@ namespace SMP
         public override void Use(Player p, params string[] args)
         {
             World w = World.Find(p.level.name);
-            
-            
 
-           
-           
 
-            if (args.Length == 1)
+
+
+
+
+            //if (args.Length == 1)
+            //{
+
+            //    if (args[0] == "off")
+            //    {
+            try
+            {
+                if (args[0] == "status") { p.SendMessage(Color.Purple + "Rain is: " + w.Israining); return; }
+            }
+            catch { }
+
+            if (w.Israining)
             {
 
-                if (args[0] == "off")
-                {
-                    foreach (Player q in Player.players)
-                    {
-                        w.rain(false, q);
-                    }
-                    p.SendMessage(Color.Red + "Stopping rain..");
-                    w.Israining = false;
-                   // p.SendMessage("rain is: " + w.isRain().ToString());
-                    
-                }
-                if (args[0] == "on")
-                {
-                    foreach (Player q in Player.players)
-                    {
-                        w.SendLightning(1, 1, 100, 2, q);
-                        w.rain(true, q);   
-                    }
-                    w.Israining = true;
-                  //  p.SendMessage("rain is: " + w.isRain().ToString());
-                    
-                    p.SendMessage(Color.Green + "Starting rain...");
-                }
-                if (args[0] == "status") { p.SendMessage(Color.Purple + "Rain is: " + w.Israining); }
-                
-              
+                w.rain(false);
+
+                p.SendMessage(Color.Red + "Stopping rain..");
+                w.Israining = false;
+
             }
-            else { Help(p); return; }
-            
+
+                   // p.SendMessage("rain is: " + w.isRain().ToString());
+
+              //  }
+            //if (args[0] == "on")
+            //{
+
+            else
+            {
+
+                w.SendLightning(1, 1, 100, 2, p);
+                w.rain(true);
+
+                w.Israining = true;
+                //  p.SendMessage("rain is: " + w.isRain().ToString());
+
+                p.SendMessage(Color.Green + "Starting rain...");
+
+
+
+                //  }
+            }
+
+
+            // }
+            //  else { Help(p); return; }
+
         }
 
         public override void Help(Player p)
         {
-           
-            p.SendMessage(Color.Blue + "/rain <on:off>");
+
+            p.SendMessage(Color.Blue + "/rain....was that so hard?");
         }
     }
 }
