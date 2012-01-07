0a9341d43cff95d21e46c39f92ec60a9e68ec102
hypereddie10 <eddie@gamezgalaxy.com>
2012-01-07 17:16:48 -0500

    Bug fixing!

--- SMP/Server/API/Crafting/Custom Recipes.cs
+++ SMP/Server/API/Crafting/Custom Recipes.cs
@@ -9,7 +9,7 @@ namespace SMP.API
     {
         CraftingBench = 9,
         PersonalInventory = 4,
-        furnace = 2
+        Furnace = 2
     }
     public abstract class Custom_Recipes
     {
@@ -22,33 +22,47 @@ namespace SMP.API
         public static void Load(Custom_Recipes c)
         {
             Item[] stack = c.GetStack();
+            if (stack == null) throw new Exception("The stack returned null");
             Crafting.CreateDefinedRecipe(c.output, stack, (short)c.size);
         }
         public Item[] GetStack()
         {
-            Item[] stack = new Item[9];
-            for (int i = 0; i < line1.Length; i++)
+            //if (size == Size.Furnace)
+            //    return new Item[] { line1[0], line2[0] };
+            if (size == Size.CraftingBench || size == Size.PersonalInventory)
             {
-                if (line1[i] == null)
-                    stack[i] = new Item(-1);
-                else
-                    stack[i] = line1[i];
+                Item[] stack = new Item[(short)size];
+                int add1 = 3;
+                if (size == Size.PersonalInventory)
+                    add1 = 2;
+                for (int i = 0; i < line1.Length; i++)
+                {
+                    if (line1[i] == null)
+                        stack[i] = new Item(-1);
+                    else
+                        stack[i] = line1[i];
+                }
+                for (int i = 0; i < line2.Length; i++)
+                {
+                    if (line2[i] == null)
+                        stack[i + add1] = new Item(-1);
+                    else
+                        stack[i + add1] = line2[i];
+                }
+                if (size == Size.CraftingBench)
+                {
+                    for (int i = 0; i < line3.Length; i++)
+                    {
+                        if (line3[i] == null)
+                            stack[i + 6] = new Item(-1);
+                        else
+                            stack[i + 6] = line3[i];
+                    }
+                }
+                return stack;
             }
-            for (int i = 0; i < line2.Length; i++)
-            {
-                if (line2[i] == null)
-                    stack[i + 3] = new Item(-1);
-                else
-                    stack[i + 3] = line2[i];
-            }
-            for (int i = 0; i < line3.Length; i++)
-            {
-                if (line3[i] == null)
-                    stack[i + 6] = new Item(-1);
-                else
-                    stack[i + 6] = line3[i];
-            }
-            return stack;
+            else
+                return null;
         }
     }
 }
